02169ff7bf0e1f863db72914cf0e7eaeb972dc4d
Gavin <kirengaboris07@gmail.com>
2025-05-04 22:40:42 +0200

    First Commit

---
+++ src/main.jsx
@@ -0,0 +1,24 @@
+import { StrictMode } from 'react'
+import { createRoot } from 'react-dom/client'
+import App from './App.jsx'
+import './index.css'
+import Layout from './Layout.jsx'
+
+import { createBrowserRouter, RouterProvider, createRoutesFromElements, Route } from 'react-router-dom'
+
+const router = createBrowserRouter(
+  createRoutesFromElements(
+    <Route path="/" element={<Layout />}>
+      <Route index element={<Home />} />
+      <Route path="about" element={<About />} />
+      <Route path="contact" element={<Contact />} />
+      <Route path="*" element={<NotFound />} />
+    </Route>
+  )
+)
+
+createRoot(document.getElementById('root')).render(
+  <StrictMode>
+    <RouterProvider router={router} />
+  </StrictMode>,
+)
